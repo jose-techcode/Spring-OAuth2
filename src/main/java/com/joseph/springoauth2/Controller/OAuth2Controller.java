dbdfb7e8c3824b50483c45f0480203342dac8a9b
José <walterandrade0001@gmail.com>
2026-03-23 15:34:14 -0300

    Feat: Addition of mandatory and essential information collection for authentication with OIDC

--- src/main/java/com/joseph/springoauth2/Controller/OAuth2Controller.java
+++ src/main/java/com/joseph/springoauth2/Controller/OAuth2Controller.java
@@ -1,8 +1,12 @@
 package com.joseph.springoauth2.Controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/")
@@ -10,7 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class OAuth2Controller {
 
     @GetMapping()
-    public String Hello() {
-        return "Hello";
+    public Map<String,Object> user(@AuthenticationPrincipal OidcUser oidcUser) {
+        if (oidcUser == null) {
+            return Map.of("authenticated", false);
+        }
+        return Map.of("name", oidcUser.getFullName(), "email", oidcUser.getEmail(), "sub", oidcUser.getSubject(), "authenticated", true);
     }
 }
